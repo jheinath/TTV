cca4d8df2a7587216775becc1a1009f75c9b4bf0
Julian Heinath <jheinath@grenkeleasing.com>
2020-03-29 17:14:15 +0200

    added shuffle to sponsoren order

--- scripts.js
+++ scripts.js
@@ -165,3 +165,10 @@ var insertFooter = () => {
   </ul>
   `;
 };
+
+const shuffleSponsoren = () => {
+  var ul = document.getElementById('sponsorenUl');
+  for (var i = ul.children.length; i >= 0; i--) {
+      ul.appendChild(ul.children[Math.random() * i | 0]);
+  }
+}
